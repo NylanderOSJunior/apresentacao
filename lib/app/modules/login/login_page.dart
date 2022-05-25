1e057f525536efd065b2276a34ff2635c019581e
NylanderOSJunior <sanguetogamer@gmail.com>
2022-05-25 12:12:14 -0300

    first commit

---
+++ lib/app/modules/login/login_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+import 'login_store.dart';
+
+class LoginPage extends StatefulWidget {
+  final String title;
+  const LoginPage({Key? key, this.title = "Login"}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ModularState<LoginPage, LoginStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: ScopedBuilder<LoginStore, Exception, int>(
+        store: store,
+        onState: (_, counter) {
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: Text('$counter'),
+          );
+        },
+        onError: (context, error) => Center(
+          child: Text(
+            'Too many clicks',
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
